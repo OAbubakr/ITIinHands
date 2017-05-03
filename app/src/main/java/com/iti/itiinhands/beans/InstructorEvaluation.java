9cc0254cd8fd2ad9436096eb5c8af91bb6fe6028
Sandra <sandranadyhelmy@gmail.com>
2017-05-03 16:15:13 +0200

    beans 6

--- app/src/main/java/com/iti/itiinhands/beans/InstructorEvaluation.java
+++ app/src/main/java/com/iti/itiinhands/beans/InstructorEvaluation.java
@@ -8,5 +8,74 @@ public class InstructorEvaluation {
 
     private int classTimeManagement;
     private int learningInspiration;
-    private int clarity
+    private int clarityAndPreparation;
+    private int participationAndDiscussion;
+    private int easeOfApproach;
+    private int interestsStimulation;
+    private int respectInTreatment;
+    private int overAllRate;
+
+    public int getClassTimeManagement() {
+        return classTimeManagement;
+    }
+
+    public void setClassTimeManagement(int classTimeManagement) {
+        this.classTimeManagement = classTimeManagement;
+    }
+
+    public int getLearningInspiration() {
+        return learningInspiration;
+    }
+
+    public void setLearningInspiration(int learningInspiration) {
+        this.learningInspiration = learningInspiration;
+    }
+
+    public int getClarityAndPreparation() {
+        return clarityAndPreparation;
+    }
+
+    public void setClarityAndPreparation(int clarityAndPreparation) {
+        this.clarityAndPreparation = clarityAndPreparation;
+    }
+
+    public int getParticipationAndDiscussion() {
+        return participationAndDiscussion;
+    }
+
+    public void setParticipationAndDiscussion(int participationAndDiscussion) {
+        this.participationAndDiscussion = participationAndDiscussion;
+    }
+
+    public int getEaseOfApproach() {
+        return easeOfApproach;
+    }
+
+    public void setEaseOfApproach(int easeOfApproach) {
+        this.easeOfApproach = easeOfApproach;
+    }
+
+    public int getInterestsStimulation() {
+        return interestsStimulation;
+    }
+
+    public void setInterestsStimulation(int interestsStimulation) {
+        this.interestsStimulation = interestsStimulation;
+    }
+
+    public int getRespectInTreatment() {
+        return respectInTreatment;
+    }
+
+    public void setRespectInTreatment(int respectInTreatment) {
+        this.respectInTreatment = respectInTreatment;
+    }
+
+    public int getOverAllRate() {
+        return overAllRate;
+    }
+
+    public void setOverAllRate(int overAllRate) {
+        this.overAllRate = overAllRate;
+    }
 }
