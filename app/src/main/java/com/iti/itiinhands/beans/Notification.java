4bff7a0cd2a24522c24ea219856ef0fe888e38c5
Sandra <sandranadyhelmy@gmail.com>
2017-05-03 14:59:38 +0200

    beans 4

---
+++ app/src/main/java/com/iti/itiinhands/beans/Notification.java
@@ -0,0 +1,12 @@
+package com.iti.itiinhands.beans;
+
+/**
+ * Created by Sandra on 5/3/2017.
+ */
+
+public class Notification {
+    public enum Type {event, scheduleChange, announcement}
+
+    private Type type;
+
+}
