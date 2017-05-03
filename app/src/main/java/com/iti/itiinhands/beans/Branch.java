0932fc6690e0276ff4cf6443b5a14c277cfd7c87
Sandra <sandranadyhelmy@gmail.com>
2017-05-03 13:23:35 +0200

    beans 2

---
+++ app/src/main/java/com/iti/itiinhands/beans/Branch.java
@@ -0,0 +1,47 @@
+package com.iti.itiinhands.beans;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Sandra on 5/3/2017.
+ */
+
+public class Branch {
+    private String location;
+    private ArrayList<Track> tracks;
+    private ArrayList<String> mapImgUrl = new ArrayList<>();
+    private ArrayList<Bus> buses = new ArrayList<>();
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public ArrayList<Track> getTracks() {
+        return tracks;
+    }
+
+    public void setTracks(ArrayList<Track> tracks) {
+        this.tracks = tracks;
+    }
+
+    public ArrayList<String> getMapImgUrl() {
+        return mapImgUrl;
+    }
+
+    public void setMapImgUrl(ArrayList<String> mapImgUrl) {
+        this.mapImgUrl = mapImgUrl;
+    }
+
+    public ArrayList<Bus> getBuses() {
+        return buses;
+    }
+
+    public void setBuses(ArrayList<Bus> buses) {
+        this.buses = buses;
+    }
+}
+
