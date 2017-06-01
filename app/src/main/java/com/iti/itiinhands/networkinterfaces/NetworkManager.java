7c117ba449e738f514a009c55b0a570bdf816d6a
Sandra <sandranadyhelmy@gmail.com>
2017-06-01 18:12:18 +0200

    announcement trial

--- app/src/main/java/com/iti/itiinhands/networkinterfaces/NetworkManager.java
+++ app/src/main/java/com/iti/itiinhands/networkinterfaces/NetworkManager.java
@@ -35,8 +35,8 @@ public class NetworkManager {
 //    private static final String BASEURL = "http://172.16.4.239:8084/restfulSpring/";
 //    private static final String BASEURL = "http://192.168.1.6:8084/restfulSpring/"; // Ragab ip and url
 
-    private static final String BASEURL = "http://192.168.43.4:8090/restfulSpring/";
-//    private static final String BASEURL = "http://10.0.2.2:8090/restfulSpring/"; // Sandra ip and url
+//    private static final String BASEURL = "http://192.168.43.4:8090/restfulSpring/";
+    private static final String BASEURL = "http://172.16.3.46:9090/restfulSpring/"; // Sandra ip and url
     private static NetworkManager newInstance;
     private static Retrofit retrofit;
 
