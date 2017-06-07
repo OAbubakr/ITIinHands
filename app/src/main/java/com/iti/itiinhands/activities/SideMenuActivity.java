ed6b1d10dd7dd802a3b3301178a9b2fff274d520
Omar <omar.abubakr.alexeng@gmail.com>
2017-06-07 17:14:03 +0200

    حبشتكنات

--- app/src/main/java/com/iti/itiinhands/activities/SideMenuActivity.java
+++ app/src/main/java/com/iti/itiinhands/activities/SideMenuActivity.java
@@ -2,6 +2,7 @@ package com.iti.itiinhands.activities;
 
 import android.content.SharedPreferences;
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
@@ -64,6 +65,7 @@ public class SideMenuActivity extends AppCompatActivity {
 
 
     UserData userData;
+    boolean doubleBackToExitPressedOnce = false;
 
 
     @Override
@@ -335,7 +337,21 @@ public class SideMenuActivity extends AppCompatActivity {
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
-            super.onBackPressed();
+            if (doubleBackToExitPressedOnce) {
+                super.onBackPressed();
+                return;
+            }
+
+            this.doubleBackToExitPressedOnce = true;
+            Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT).show();
+
+            new Handler().postDelayed(new Runnable() {
+
+                @Override
+                public void run() {
+                    doubleBackToExitPressedOnce=false;
+                }
+            }, 2000);
         }
     }
 
@@ -350,4 +366,9 @@ public class SideMenuActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         LISessionManager.getInstance(getApplicationContext()).onActivityResult(this,requestCode, resultCode, data);
     }
+
+//    @Override
+//    public void onBackPressed() {
+//
+//    }
 }
