b89f86c34a2c4a225a6d897c1a3672864cc92562
Rana Gamal <rana.gamal.brekaa@gmail.com>
2017-06-10 22:33:35 +0200

    Post Job Validation Again

--- app/src/main/java/com/iti/itiinhands/networkinterfaces/NetworkManager.java
+++ app/src/main/java/com/iti/itiinhands/networkinterfaces/NetworkManager.java
@@ -63,7 +63,7 @@ public class NetworkManager {
 //    private static final String BASEURL = "http://172.16.2.40:8085/restfulSpring/"; // Ragab ip and url
 //    private static final String BASEURL = "http://172.16.3.46:9090/restfulSpring/"; // Omar ITI
 //    private static final String BASEURL = "http://192.168.1.17:8085/restfulSpring/"; // Omar ITI
-    private static final String BASEURL = "http://172.16.2.218:8084/restfulSpring/";
+    private static final String BASEURL = "http://192.168.1.103:8084/restfulSpring/";
 
     private static NetworkManager newInstance;
     private static Retrofit retrofit;
