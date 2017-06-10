5268e15879f6c2902f16c260df724b5f64ff0d3d
Mahmoud <engmahmoudamin14@gmail.com>
2017-06-10 16:11:29 +0200

    Mahmoud
    tint color

---
+++ app/src/main/java/com/iti/itiinhands/adapters/EventsPagerAdapter.java
@@ -0,0 +1,41 @@
+package com.iti.itiinhands.adapters;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import com.iti.itiinhands.fragments.EventCalendarFragment;
+import com.iti.itiinhands.fragments.EventListFragment;
+
+/**
+ * Created by admin on 6/10/2017.
+ */
+
+public class EventsPagerAdapter extends FragmentStatePagerAdapter {
+
+    int num;
+    public  EventsPagerAdapter(int num,FragmentManager fm){
+        super(fm);
+        this.num=num;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+
+        switch(position){
+
+            case 1:
+                return new EventListFragment();
+            case 2:
+            return new EventCalendarFragment();
+
+            default: return null;
+        }
+
+    }
+
+    @Override
+    public int getCount() {
+        return num;
+    }
+}
