05b32f413ce2086511e7a5f7c5990ca75fd6772e
Sandra <sandranadyhelmy@gmail.com>
2017-06-12 16:19:52 +0200

    Merge branch 'master' of https://github.com/OAbubakr/ITIinHands
    Conflicts:
    	app/src/main/java/com/iti/itiinhands/networkinterfaces/NetworkManager.java
    	linkedin-sdk/build/intermediates/blame/res/release/single/drawable-hdpi-v4.json
    	linkedin-sdk/build/intermediates/blame/res/release/single/drawable-mdpi-v4.json
    	linkedin-sdk/build/intermediates/blame/res/release/single/drawable-xhdpi-v4.json
    	linkedin-sdk/build/intermediates/blame/res/release/single/drawable-xxhdpi-v4.json
    	linkedin-sdk/build/intermediates/incremental/mergeReleaseAssets/merger.xml
    	linkedin-sdk/build/intermediates/incremental/mergeReleaseJniLibFolders/merger.xml
    	linkedin-sdk/build/intermediates/incremental/mergeReleaseResources/compile-file-map.properties
    	linkedin-sdk/build/intermediates/incremental/mergeReleaseResources/merger.xml
    	linkedin-sdk/build/intermediates/incremental/mergeReleaseShaders/merger.xml
    	linkedin-sdk/build/intermediates/incremental/packageReleaseResources/compile-file-map.properties
    	linkedin-sdk/build/intermediates/incremental/packageReleaseResources/merger.xml
    	linkedin-sdk/build/outputs/aar/linkedin-sdk-release.aar

--- app/src/main/java/com/iti/itiinhands/networkinterfaces/NetworkManager.java
+++ app/src/main/java/com/iti/itiinhands/networkinterfaces/NetworkManager.java
@@ -55,7 +55,7 @@ public class NetworkManager {
     public static final String BASEURL = "http://172.16.3.46:9090/restfulSpring/";
 
 //    private static final String BASEURL = "http://192.168.43.4:8090/restfulSpring/";
-    public static final String BASEURL = "http://172.16.2.40:8085/restfulSpring/";
+//    public static final String BASEURL = "http://172.16.2.40:8085/restfulSpring/";
     private static NetworkManager newInstance;
     private static Retrofit retrofit;
     private static final String API_KEY_BEHANCE = "SXf62agQ8r0xCNCSf1q30HJMmozKmAFA";
